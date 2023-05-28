b580c3d1c70622ef4b6bf6ece3a31e1d244fc44d
Vlad <vladislav.stan.1998@gmail.com>
2023-05-28 15:19:54 +0300

    I deleted it because it was unnecessary

--- src/components/pages/index.js
+++
@@ -1,6 +0,0 @@
-import MainPage from './MainPage';
-import ComicsPage from './ComicsPage';
-import SingleComicsPage from './SingleComicsPage';
-import NotFoundPage from './NotFoundPage';
-
-export { MainPage, ComicsPage, SingleComicsPage, NotFoundPage };
